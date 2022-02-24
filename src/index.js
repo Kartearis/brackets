219913f569984b2ceb5dbcb00c8fb5971a242265
Matveev Timofey <justshowel@gmail.com>
2022-02-24 18:20:22 +0300

    feat: Implement bracket checking.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let opening = bracketsConfig.map(el => el[0]);
+  let closing = bracketsConfig.map(el => el[1]);
+  for (let symbol of str) {
+      if (closing.includes(symbol) && closing[opening.findIndex(el => el === stack[stack.length - 1])] === symbol)
+          stack.pop();
+      else if (opening.includes(symbol))
+          stack.push(symbol);
+      else return false;
+  }
+  if (stack.length === 0)
+      return true;
+  return false;
 }
+
+console.log(module.exports("(||){|({})|}", [['(', ')'], ['{', '}'],['|','|']]));
